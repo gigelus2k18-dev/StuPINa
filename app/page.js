d0dd1ef6ffc73682fb95e3bd612911a45100fcd7
gigelus2k18-dev <gigelus2k18@gmail.com>
2026-09-19 11:47:06 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🐝 StuPINa</h1>
+      <p>Administrarea stupinei</p>
+
+      <div style={{ marginTop: "30px" }}>
+        <h2>Stupii mei</h2>
+        <p>În curând aici vor apărea cei 200 de stupi.</p>
+      </div>
+    </main>
+  );
+}
