dca17b8dff1144670b4f20295fdb586c70222f5d
gigelus2k18-dev <gigelus2k18@gmail.com>
2026-09-19 12:29:13 +0300

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(
+  /\/rest\/v1\/?$/,
+  ""
+);
+
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseUrl,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
